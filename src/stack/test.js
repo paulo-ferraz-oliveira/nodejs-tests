b21cbd6de06ac589a4db13f62a4c2d42793782ab
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2023-06-21 00:17:11 +0100

    Add stack tests (and improve test framework)
    Copies the concept of cfg over from Common Test.
    
    Thanks again, Erlang
    
    Adds is_exception to the assertions
    
    Fixes a minor bug with logging (and starts proper logging of
    stacktraces)

---
+++ src/stack/test.js
@@ -0,0 +1,79 @@
+const assert = require('../../test/utils/assert.js')
+const is = assert.is
+const is_exception = assert.is_exception
+
+const stack = require('.')
+const Stack = stack.Stack
+
+function before() {
+  return { stack: new Stack() }
+}
+
+function returns_true_when_empty(cfg) {
+  is(true, cfg.stack.empty())
+}
+
+function returns_false_when_not_empty(cfg) {
+  cfg.stack.push(1)
+  is(false, cfg.stack.empty())
+}
+
+function throws_on_empty_stack_peek(cfg) {
+  is_exception(stack.EmptyStackException, () => {
+    cfg.stack.peek()
+  })
+}
+
+function allows_peek_when_not_empty(cfg) {
+  var pushed = 1
+  cfg.stack.push(pushed)
+  is(pushed, cfg.stack.peek())
+}
+
+function peek_shows_correct_value(cfg) {
+  cfg.stack.push(1)
+  cfg.stack.push(2)
+  is(2, cfg.stack.peek())
+  cfg.stack.pop()
+  is(1, cfg.stack.peek())
+}
+
+function throws_on_empty_stack_pop(cfg) {
+  is_exception(stack.EmptyStackException, () => {
+    cfg.stack.pop()
+  })
+}
+
+function pop_returns_correct_value_and_updates_stack(cfg) {
+  var pushed = 1
+  cfg.stack.push(pushed)
+  is(pushed, cfg.stack.pop())
+  is(true, cfg.stack.empty())
+}
+
+function push_puts_correct_value_in_stack(cfg) {
+  var pushed
+  is(true, cfg.stack.empty())
+  pushed = 1
+  cfg.stack.push(pushed)
+  is(pushed, cfg.stack.peek())
+  pushed = 2
+  cfg.stack.push(pushed)
+  is(pushed, cfg.stack.peek())
+}
+
+module.exports = {
+  before: before,
+  all: () => {
+    return [
+      ['Returns true when stack is empty', returns_true_when_empty],
+      ['Returns false when stack is not empty', returns_false_when_not_empty],
+      ['Throws on empty stack .peek()', throws_on_empty_stack_peek],
+      ['Allows peek when not empty', allows_peek_when_not_empty],
+      ['Peek shows correct value', peek_shows_correct_value],
+      ['Throws on empty stack .pop()', throws_on_empty_stack_pop],
+      ['Pop works as expected and updates stack', pop_returns_correct_value_and_updates_stack],
+      ['Push works as expected and updates stack', push_puts_correct_value_in_stack],
+    ]
+  },
+}
