18c26a4e11b106bc3e6dd3629238037508c687ff
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2024-07-14 23:47:23 +0100

    Improve on ESLint's config.

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+
+export default [
+  {
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'script' },
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  pluginJs.configs.recommended,
+]
